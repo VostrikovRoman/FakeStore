b7d85370a144bc7259cb651de67fe874a3c3e5cf
FroStyRo <62169048+VostrikovRoman@users.noreply.github.com>
2025-01-04 11:55:28 +0500

    Работа кипит

---
+++ src/components/store_card.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="product_card">
+        <img :src="image" class="product_img">
+        <p>{{ title }}</p>
+    </div>
+</template>
+
+<script setup>
+import {defineProps} from 'vue'
+
+defineProps({
+  id: Number,
+  title: String,
+  price: Number,
+  description: String,
+  category: String,
+  image: String,
+  rate: Number,
+  count: Number
+})
+
+</script>
+
+<style>
+.product_card{
+    background: var(--grey);
+    display: inline-block;
+    box-shadow: inset 0px 0px 5px 0px;
+    margin: 10px;
+    padding: 10px;
+}
+.product_img{
+    width: 100%;
+}
+
+</style>
