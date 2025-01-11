682550db618d4038a028309aba374a2b5e12bfea
FroStyRo <62169048+VostrikovRoman@users.noreply.github.com>
2025-01-11 14:02:31 +0500

    Начал настраивать окно товара. Есть проблема - выдаёт ошибку до тех пор, пока не будет нажата кнопка Категории

--- src/components/categories_bar.vue
+++ src/components/categories_bar.vue
@@ -1,18 +1,28 @@
 <template>
     <div>
         <h1 class="text title">Категории товаров</h1>
-        <p class="text list" v-for="category in categories" :key="category" style="cursor:pointer;">{{category.replace(category[0], category[0].toUpperCase())}}</p>
+        <p class="text list" v-for="category in categories" :key="category" style="cursor:pointer;"
+        @click="GetDataCategory(category)"
+        >{{category.replace(category[0], category[0].toUpperCase())}}</p>
     </div>
 </template>
 
 <script setup>
+import {defineProps} from 'vue'
+import {ref} from 'vue'
 
-let categories = [
-  "electronics",
-  "jewelery",
-  "men's clothing",
-  "women's clothing"
-]
+
+
+defineProps({
+  categories:String
+})
+function GetDataCategory(c){
+    let data = ref(null)
+  fetch(`https://fakestoreapi.com/products/category/${c}`)
+            .then(res=>res.json())
+            .then(json=>data.value = json)
+    
+}
 
 </script>
 
---
+++ src/components/trade_elem.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="window hide product_background">
+        <div class="trade">
+            
+        </div>
+    </div>
+    
+</template>
+
+<script setup>
+import {defineProps} from 'vue'
+
+defineProps({
+  id: Number,
+  title: String,
+  price: Number,
+  description: String,
+  category: String,
+  image: String,
+  rate: Number,
+  count: Number
+})
+
+</script>
+
+<style>
+.trade{
+    width: 100%;
+    font-family: 'Caviar Dreams Bold';
+    z-index: 12;
+    background: var(--white);
+    margin: 15px;
+    border-radius: 5px;
+}
+.product_background{
+    background: #000000a8;
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    z-index: 12;
+    display: flex;
+}
+</style>
